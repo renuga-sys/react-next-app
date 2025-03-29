df82aba9f9b83051ee334a737d94d6a686fce543
Renuga Krishnan <kmsg.renu@gmail.com>
2025-03-29 18:42:04 +0000

    removed unused icons

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,13 @@ import ProductCard from './Components/ProductCard/ProductCard'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-<h1 className="text-3xl font-bold underline">
-  Hello world!
-  <Link href="/UserDetails" className="card w-96 bg-base-100 shadow-xl">Show Users </Link>
-<ProductCard />
-</h1>
+    <main className="flex min-h-screen flex-col items-center p-24">
+<h1 className="text-3xl font-bold underline p-16"> Cart Application </h1>
+
+  <div className="grid grid-cols-3 gap-4 border border-slate-400">
+  <Link href="/Products" className="card w-96 bg-base-100">Show Products </Link>
+  <Link href="/UserDetails" className="card w-96 bg-base-100">Show Users </Link>
+</div>
     </main>
   )
 }
